2bce79c37896b6a05c58922bdccaa81428e0111a
Yizhao He <heyizhaobupt@gmail.com>
2017-04-09 12:11:14 -0700

    finish generate animation

---
+++ backend/public/javascripts/generate.js
@@ -0,0 +1,89 @@
+var app = new Vue({
+  el: '#root',
+  data: {
+    sentence: '',
+    prime: '',
+    show: false
+  },
+  methods: {
+    checkSentence: function () {
+      if (this.sentence.trim() == '') {
+        $('#sentence').popup('show')
+        setTimeout(function () {
+          $('#sentence').popup('hide')
+        }, 5000)
+        return false
+      } else {
+        return true
+      }
+    },
+    checkPrime: function () {
+      if (this.prime.trim() == '') {
+        $('#prime').popup('show')
+        setTimeout(function () {
+          $('#prime').popup('hide')
+        }, 5000)
+        return false
+      } else {
+        return true
+      }
+    },
+    doit: function () {
+      let a = this.checkPrime()
+      let b = this.checkSentence()
+      let me = this
+      if (a && b) {
+        $('#root .pg-output li').remove()
+        me.show = true
+        let url = '/generate/sentence/' + this.sentence.trim() + '/prime/' + this.prime.trim()
+        $.getJSON(url, function (data) {
+          me.show = false
+          for (let i = 0; i < data.result.length; i++) {
+            let len = data.result[i].length
+            let end = i == data.result.length - 1 ? '。' : '，'
+
+            insert(i, data.result[i] + end, i * (len + 3) * 300)
+          }
+
+          function insert(index, line, delay) {
+            setTimeout (function () {
+              $('#root .pg-output').append('<li class="tlt' + index + '">' + line + '</li>')
+              $('.tlt' + index).textillate({ 
+                in: { 
+                  effect: 'fadeIn',
+                  delay: 300
+                } 
+              })
+            }, delay)
+          }
+        })
+        .fail(function () {
+          me.show = false
+          alert('Generation failed!')
+        })
+      }
+    }
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      $('#sentence')
+        .popup({
+          content: 'Please type the first sentence',
+          variation: 'inverted',
+          on: 'manual'
+        })
+      $('#prime')
+        .popup({
+          content: 'Please type the prime keyword',
+          variation: 'inverted',
+          on: 'manual'
+        })
+      // $('.tlt').textillate({ 
+      //   in: { 
+      //     effect: 'fadeIn',
+      //     delay: 300
+      //   } 
+      // })
+    })
+  }
+})
